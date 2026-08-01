d4750868f53298772f2588261276efc553c45e2a
Debashis Saikia <saikiadebashis5@gmail.com>
2026-08-02 01:09:19 +0530

    Update Schedule.tsx

--- src/components/Schedule.tsx
+++ src/components/Schedule.tsx
@@ -43,7 +43,7 @@ export function Schedule() {
   };
 
   return (
-    <SectionBackground className="py-24">
+    <section id="schedule" className="py-24 bg-background">
       <div className="container mx-auto px-4 md:px-6 max-w-5xl">
         <motion.div 
           initial={{ opacity: 0, y: 20 }}
@@ -88,6 +88,6 @@ export function Schedule() {
           ))}
         </div>
       </div>
-    </SectionBackground>
+    </section>
   );
 }
